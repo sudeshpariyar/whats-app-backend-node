4ffd9c4c40141b7b5dc7fb24488226a4442a915b
sudeshpariyar <sudesh.bullet@gmail.com>
2020-10-15 18:53:53 +1100

    whats app backend in node.js

---
+++ server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher"
+import cors from "cors"
+// app config
+const app = express()
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: '1089346',
+  key: '86b79da6459b092fc3dc',
+  secret: 'cfbd62f9fa52a059fcd8',
+  cluster: 'ap4',
+  encrypted: true
+});
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+// db config
+//su4RmKqy5OdQPjml
+
+const connection_url='mongodb+srv://admin:su4RmKqy5OdQPjml@cluster0.mgp0u.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+mongoose.connect(connection_url,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+const db= mongoose.connection;
+
+db.once("open",()=>{
+    console.log("DB connected");
+
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on('change',(change)=>{
+        console.log("change occour",change);
+
+        if(change.operationType ==='insert'){
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages','inserted',{
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,
+            });
+        }else{
+            console.log('Error triggering Pusher')
+        }
+    })
+})
+// api routes
+app.get('/',(req,res)=>res.status(200).send("hello world"));
+
+app.get('messages/sync',(req,res)=>{
+    Messages.find((err,data)=>{
+        if (err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new',(req,res)=>{
+    const dbMessage = req.body
+
+    dbMessages.create(dbMessage, (err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+//listen
+app.listen(port,()=>console.log(`Listining on localhost:${port}`));
